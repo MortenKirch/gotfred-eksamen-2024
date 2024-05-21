37ccaea7f034150e391b9d5678c65051741902cf
MortenKirch <morten-kris@live.dk>
2024-05-21 13:27:14 +0200

    javscript til om gotfred

--- javascript/image-swap-om-os.js
+++ javascript/image-swap-om-os.js
@@ -3,7 +3,7 @@
 // DOMContentLoaded, sørger for at den loader content på siden først
 document.addEventListener("DOMContentLoaded", function() {
 //    forskellige billede sources i et array,
-    let images = ["/pictures/bryllupskage.jpg", "/pictures/billede-catering-2.png", "/pictures/billede-catering-3.png"];
+    let images = ["/pictures/infographic/ig-aeblekage.webp","/pictures/infographic/ig-banoffee.webp","/pictures/infographic/ig-fragilite.webp","/pictures/infographic/ig-hindbaer.webp","/pictures/infographic/ig-peach.webp","/pictures/infographic/ig-yuzu.webp"];
     let currentIndex = 0;
     let currentImage = document.getElementById('image-swap-om-os');
 
@@ -18,8 +18,8 @@ document.addEventListener("DOMContentLoaded", function() {
 
             // Fade in ny image
             currentImage.style.opacity = 1;
-        }, 1000); // css transtision duration/tid
+        }, 1500); // css transtision duration/tid
     }
     // længden mellem hvert image swap, 5000ms/5 sek
-    setInterval(changeImage, 5000);
+    setInterval(changeImage, 10000);
 });
